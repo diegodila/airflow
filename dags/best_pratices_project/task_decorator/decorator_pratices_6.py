487b8ac233788a6ae2c91fd0da84a8f379b8ac23
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2026-03-26 21:52:44 -0300

    adding info airflow xcom

--- dags/best_pratices_project/task_decorator/decorator_pratices_6.py
+++ dags/best_pratices_project/task_decorator/decorator_pratices_6.py
@@ -1,6 +1,7 @@
-from airflow.sdk import dag
+from airflow.sdk import dag, task_group
 from airflow.utils.trigger_rule import TriggerRule
 import pendulum
+from airflow.providers.standard.operators.empty import EmptyOperator
 from best_pratices_project.task_decorator.task_pratices_6 import OperatorsFactory
 
 @dag(
@@ -16,6 +17,15 @@ def my_dag():
     t1 = OperatorsFactory("meu_id_1", "sleep 10", TriggerRule.ALL_DONE).build("bash")
     t2 = OperatorsFactory("meu_id_2","Diego",TriggerRule.ALL_DONE).build("python")
     
+    @task_group(group_id='load_database')
+    def tg1():
+        t3 = EmptyOperator(task_id='task_group3')
+        t4 = EmptyOperator(task_id='task_group4')
+        t5 = OperatorsFactory("meu_id_5", "echo 'task group aqui'", TriggerRule.ALL_DONE).build("bash")
+        t6 = OperatorsFactory("meu_id_6", "hello taskgroup", TriggerRule.ALL_DONE).build("python")
+        [t3 >> t4 >> t5] >> t6
     
+    
+    t1 >> tg1() >> t2
 dag = my_dag()
     
